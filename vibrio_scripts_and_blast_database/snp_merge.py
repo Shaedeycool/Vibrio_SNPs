206fc85bf539ebdf2bc63cee04d8a7a70d1bc52b
Sade_Monica_Magabotha <51709731+Shaedeycool@users.noreply.github.com>
2025-06-26 10:35:56 +0200

    Add files via upload

--- vibrio_scripts_and_blast_database/snp_merge.py
+++ vibrio_scripts_and_blast_database/snp_merge.py
@@ -12,12 +12,12 @@ df_1 = [pd.read_csv(file) for file in files]
 merged_df = pd.concat(df_1, axis=0, ignore_index=True, sort=False)
 
 # Create dictionary to rename value names of grouped_by_sample
-#rename_map = {'dbj|AB012956.1|' : 'wbfo139',  'gb|KP187623.1|' : 'tcpA_ElTor',  'gb|M33514.1|' : 'tcpA_Classic',  'gb|KC152957.1|' : 'wbeO1_ElTor',  'gb|KF498634.1|' : 'toxR',  'gb|AF463401.1|' : 'ctxA',  'gb|JF284685.1|' :  'wbeT_ElTor'}
-rename_map = {'dbj|AB012956.1|' : 'wbfo139',  'gb|KP187623.1|' : 'tcpA_ElTor',  'gb|M33514.1|' : 'tcpA_Classic',  'gb|KC152957.1|' : 'wbeO1_ElTor',  'gb|KF498634.1|' : 'toxR',  'gb|AF463401.1|' : 'ctxA'}
+#rename_map = {'dbj|AB012956.1|' : 'wbfo139',  'gb|KP187623.1|' : 'tcpA_ElTor_26975',  'gb|M33514.1|' : 'tcpA_Classic',  'gb|KC152957.1|' : 'wbeO1_ElTor',  'gb|KF498634.1|' : 'toxR',  'gb|AF463401.1|' : 'ctxA',  'gb|JF284685.1|' :  'wbeT_ElTor'}
+rename_map = {'dbj|AB012956.1|' : 'serogroup_O139_wbf',  'gb|KP187623.1|' : 'tcpA_ElTor_26975',  'gb|M33514.1|' : 'tcpA_Classic',  'gb|KC152957.1|' : 'serogroup_O1_wbe',  'gb|KF498634.1|' : 'toxR_V.choleraeSpecies',  'gb|AF463401.1|' : 'ctxA_CholeraeToxin', '1' : 'tcpA_ElTor_N16961'}
 
 # Define DataFrame column names
 #df_columns = ['sampleID', 'dbj|AB012956.1|',  'gb|KP187623.1|',  'gb|M33514.1|',  'gb|KC152957.1|',  'gb|KF498634.1|',  'gb|AF463401.1|',  'gb|JF284685.1|']
-df_columns = ['sampleID', 'dbj|AB012956.1|',  'gb|KP187623.1|',  'gb|M33514.1|',  'gb|KC152957.1|',  'gb|KF498634.1|',  'gb|AF463401.1|']
+df_columns = ['sampleID', 'dbj|AB012956.1|',  'gb|KP187623.1|',  'gb|M33514.1|',  'gb|KC152957.1|',  'gb|KF498634.1|',  'gb|AF463401.1|', '1']
 
 # Create empty DataFrame with defined column names
 df_2 = pd.DataFrame(columns=df_columns)
@@ -34,6 +34,14 @@ final_df = final_df.fillna("undetected")
 # Rename columns using dictionary
 df_3 = final_df.rename(columns=rename_map)
 
+# Dictionary defining the order of columns
+column_order = {'sampleID' : 0, 'serogroup_O139_wbf' : 7, 'tcpA_ElTor_26975' : 4, 'tcpA_Classic' : 5, 'serogroup_O1_wbe' : 6, 'toxR_V.choleraeSpecies' : 1, 'ctxA_CholeraeToxin' : 2, 'tcpA_ElTor_N16961' : 3}
+
+# Sort and reorder DataFrame columns by values in dictionary
+sort_column_order = sorted(column_order, key=column_order.get)
+
+df_3 = df_3[sort_column_order]
+
 # Save to a complete table
 df_3.to_csv("vibrio_cholerae_snps/vch_snp_table.txt", sep = '\t', index = None)
-
+df_3.to_excel("vibrio_cholerae_snps/vch_snp_table.xlsx", index = None)
---
+++ vibrio_scripts_and_blast_database/vibrio_snps_2.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ $# != 2 ];
+ then
+        echo "Usage: $0 <WorkDirectory> <RunFolder>"
+        exit
+fi
+
+workspace=$1
+runname=$2
+reportsDir=$1"/Results-"$2
+workdir=$1/"vibrio_cholerae_snps"
+
+cd ${workspace}/${runname}
+
+mkdir vibrio_cholerae_snps
+
+output_dir="vibrio_cholerae_snps"
+
+for file in assemblies/*/*_assembly.fasta; do
+## Extract sample name from the filename (e.g., YA00570981)
+    sample=$(basename "$file" | cut -d'_' -f1)
+
+## Define input and output file paths
+    input_file="$file"
+    output_file="${sample}_blast_output.xml"
+
+## Run blastn
+    blastn -query "$input_file" -db ${workspace}/Vibrio_SNPs/vibrio_scripts_and_blast_database/db/vcO1_markers/vcO1_markers -out $output_dir/"$output_file" -outfmt 5
+done
+
+
+for file in $output_dir/*_blast_output.xml; do
+## Extract sample name from the filename (e.g., YA00570981)
+    sample=$(basename "$file" | cut -d'_' -f1)
+
+## Define input and output file paths
+    input_file="$file"
+    output_file="${sample}_blast_output.txt"
+
+## Run your Python script
+    python ${workspace}/Vibrio_SNPs/vibrio_scripts_and_blast_database/xml2snp_2.py -i "$input_file" -o $output_dir/"$output_file"
+done
+
+## Run Python script to make a complete table
+python ${workspace}/Vibrio_SNPs/vibrio_scripts_and_blast_database/snp_merge.py
+
+cp ${output_dir}/vch_snp_table.xlsx .
+mv vch_snp_table.xlsx Vibrio_cholerae_assembly_results.xlsx
